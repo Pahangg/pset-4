9471f3074c9b641cec7ee0ddfac43035b301419b
Paniz Ahang <pahang@ucvts.org>
2019-11-21 11:10:50 -0500

    reverse

--- src/reverse.js
+++ src/reverse.js
@@ -1,8 +1,26 @@
 const readlineSync = require("readline-sync");
 
-let Number = 0;
+let value = 0;
 
-console.log("")
+console.log();
 do {
-     integer = Number(readlineSync.question("Positive integer: "));
-} while (integer < 1 || integer > Number.MAX_SAFE_INTEGER || Number.isNaN(integer) || !Number.isInteger(integer));
+     value = Number(readlineSync.question("Positive integer: "));
+} while (value < Number.MIN_SAFE_INTEGER ||
+         value > Number.MAX_SAFE_INTEGER ||
+         Number.isNaN(value) ||
+         !Number.isInteger(value));
+
+let digits = "";
+while (value > 0) {
+  const digit = value % 10;
+  value = Math.floor(value / 10);
+
+  if(value > 0){
+    digits = digits + digit + ", ";
+  }else{
+    digits = digits + digit + ".";
+  }
+
+}
+
+console.log(digits);
--- src/sum.js
+++ src/sum.js
@@ -3,26 +3,35 @@ const readlineSync = require("readline-sync");
 let sum = 0;
 let x = 0;
 
-console.log("")
+console.log();
 do {
      lowerBound = Number(readlineSync.question("Lower bound: "));
      upperBound = Number(readlineSync.question("Upper bound: "));
-} while (lowerBound < Number.MIN_SAFE_INTEGER || lowerBound > Number.MAX_SAFE_INTEGER || upperBound < Number.MIN_SAFE_INTEGER || upperBound > Number.MAX_SAFE_INTEGER || lowerBound >= upperBound || Number.isNaN(lowerBound) || Number.isNaN(upperBound) || !Number.isInteger(lowerBound) || !Number.isInteger(upperBound));
+} while (lowerBound < Number.MIN_SAFE_INTEGER ||
+         lowerBound > Number.MAX_SAFE_INTEGER ||
+         upperBound < Number.MIN_SAFE_INTEGER ||
+         upperBound > Number.MAX_SAFE_INTEGER ||
+         lowerBound >= upperBound ||
+         Number.isNaN(lowerBound) ||
+         Number.isNaN(upperBound) ||
+         !Number.isInteger(lowerBound) ||
+         !Number.isInteger(upperBound));
 
+
+// while (lowerBound <= upperBound) {
+//   if (lowerBound % 2 === 0) {
+//     sum = sum + lowerBound;
+//   }
+//
+//   lowerBound++;
+// }
 
 for (let x = lowerBound; x <= upperBound; x++) {
-      if (x % 2 === 0) {
-          for (x = lowerBound; x <= upperBound; x+=2) {
-            sum = x + sum;
-          }
-      }
-
-      if (x % 2 !== 0) {
-          for (x = lowerBound + 1; x <= upperBound; x+=2) {
-            sum = x + sum;
-          }
-      }
+  if (x % 2 === 0) {
+    sum = sum + x;
+  }
 }
+
 sum = sum.toLocaleString('en');
 
 console.log("\n" + sum + ".\n")
